6e15cabbdd566806bc5c63edf4ac9d438865613e
Junseok Park <21700302@handong.edu>
2020-04-12 00:41:10 +0900

    Added Save and load functions in manager.c

--- manager.c
+++ manager.c
@@ -15,3 +15,31 @@ int selectDataNo (Product *s, int count) {
 	scanf("%d",&no);
 	return no;
 }
+
+void saveData (Product*s, int count) {
+        FILE *fp;
+        fp = fopen("menu.txt","wt");
+
+        for(int i =0; i<count; i++){
+                if (s[i].price !=-1)
+                fprintf(fp,"%d %d %f %d %s\n", s[i].price, s[i].mass, s[i].stdPrice, s[i].star, s[i].name);
+        }
+}
+int loadData (Product*s) {
+        int count = 0;
+        FILE *fp;
+        fp = fopen("menu.txt","rt");
+        if(fp==NULL){
+        printf("=>No File \n");
+        return 0;
+        }
+        for (;;count++){
+        fscanf(fp,"%d %d %f %d %[^\n]s", &s[count].price, &s[count].mass, &s[count].stdPrice, &s[count].star, s[count].name);
+
+        if (feof(fp)) break;
+}
+        fclose(fp);
+        printf("Load Success\n");
+        return count;
+}
+
